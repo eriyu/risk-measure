8b53fdd18b6d2145839996f42e25b7abd13c8de0
lala.lee <lala.lee@mitake.com.tw>
2019-09-04 21:19:03 +0800

    feat: 增加 font-awsome 及 pure-button 做畫面美化

--- src/app/konva-sample/index/index.component.ts
+++ src/app/konva-sample/index/index.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import Konva from 'konva';
+import { posix } from 'path';
 
-enum EditorMode {
-  Brush,
-  Text,
-  Arrow
+enum ToolMode {
+  Line,
+  Square,
+  Circle,
+  Arrow,
+  Text
 }
 
 enum LayerMode {
-  Both,
   Image,
   Editor
 }
@@ -20,16 +22,29 @@ enum LayerMode {
 })
 export class IndexComponent implements OnInit {
 
-  EditorMode = EditorMode;
+
+  ToolMode = ToolMode;
   LayerMode = LayerMode;
+
+  layers = [
+    {mode: LayerMode.Image, name: '底圖' , checked: true},
+    {mode: LayerMode.Editor, name: '使用者作畫的內容', checked: true},
+  ];
+
+  tools = [
+    {mode: ToolMode.Line, name: '畫線' },
+    {mode: ToolMode.Square, name: '畫框' },
+    {mode: ToolMode.Circle, name: '畫圓' },
+    {mode: ToolMode.Arrow, name: '箭頭' },
+    {mode: ToolMode.Text, name: '文字' },
+  ];
 
   stage: Konva.Stage;
   imageLayer: Konva.Layer;
   editorLayer: Konva.Layer;
 
   imageLayerLoad = false;
-  selectedMode =  EditorMode.Brush;
-  selectedLayer = LayerMode.Both;
+  selectedTool =  ToolMode.Line;
 
   isPaint = false;
   lastLine;
@@ -44,17 +59,48 @@ export class IndexComponent implements OnInit {
     this.editorLayer = new Konva.Layer();
     this.stage.add(this.imageLayer);
     this.stage.add(this.editorLayer);
+    this.initStage();
 
-    this.stage.on('mousedown touchstart', (e) => {
+    // this.stage.on('mousedown touchstart', (e) => {
+    //   this.isPaint = true;
+    //   const pos = this.stage.getPointerPosition();
+    //   this.lastLine = new Konva.Line({
+    //     stroke: '#000000',
+    //     strokeWidth: 5,
+    //     globalCompositeOperation: 'source-over',
+    //     points: [pos.x, pos.y]
+    //   });
+    //   this.editorLayer.add(this.lastLine);
+    // });
+
+    // this.stage.on('mouseup touchend', () => {
+    //   this.isPaint = false;
+    // });
+
+    // // and core function - drawing
+    // this.stage.on('mousemove touchmove', () => {
+    //   if (!this.isPaint) {
+    //     return;
+    //   }
+
+    //   const pos = this.stage.getPointerPosition();
+    //   const newPoints = this.lastLine.points().concat([pos.x, pos.y]);
+    //   this.lastLine.points(newPoints);
+    //   this.editorLayer.batchDraw();
+    // });
+  }
+
+  initStage() {
+    this.stage.on('mousedown touchstart', () => {
       this.isPaint = true;
       const pos = this.stage.getPointerPosition();
-      this.lastLine = new Konva.Line({
-        stroke: '#000000',
-        strokeWidth: 5,
-        globalCompositeOperation: 'source-over',
-        points: [pos.x, pos.y]
-      });
-      this.editorLayer.add(this.lastLine);
+      switch (+this.selectedTool) {
+        case ToolMode.Text:
+          this.createTextNode(pos);
+          break;
+        default:
+          break;
+      }
     });
 
     this.stage.on('mouseup touchend', () => {
@@ -66,11 +112,7 @@ export class IndexComponent implements OnInit {
       if (!this.isPaint) {
         return;
       }
-
       const pos = this.stage.getPointerPosition();
-      const newPoints = this.lastLine.points().concat([pos.x, pos.y]);
-      this.lastLine.points(newPoints);
-      this.editorLayer.batchDraw();
     });
   }
 
@@ -122,19 +164,6 @@ export class IndexComponent implements OnInit {
     }
   }
 
-  onEditorModeSelected(selectedMode) {
-    switch (+selectedMode) {
-      case EditorMode.Text:
-        this.createTextNode();
-        break;
-      case EditorMode.Arrow:
-        this.createArrowNode();
-        break;
-      default:
-        break;
-    }
-  }
-
   createArrowNode() {
     console.log('createTextNode');
     const arrow = new Konva.Arrow({
@@ -153,12 +182,12 @@ export class IndexComponent implements OnInit {
     this.editorLayer.draw();
   }
 
-  createTextNode() {
+  createTextNode(pos) {
     console.log('createTextNode');
     const textNode = new Konva.Text({
-      text: 'Some text here',
-      x: 10,
-      y: 10,
+      text: 'Input text here',
+      x: pos.x,
+      y: pos.y,
       fontSize: 20,
       draggable: true,
       width: 200
