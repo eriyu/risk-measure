facdda59c2d8719406af0de08de7443c691a52c1
lala.lee <lala.lee@mitake.com.tw>
2019-09-05 15:33:08 +0800

    feat: 完成所有工具的初階功能

--- src/app/konva-sample/index/index.component.ts
+++ src/app/konva-sample/index/index.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import Konva from 'konva';
-import { posix } from 'path';
+import * as _ from 'lodash';
 
 enum ToolMode {
   Line,
-  Square,
+  Rect,
   Circle,
   Arrow,
   Text
 }
 
 enum LayerMode {
-  Image,
-  Editor
+  BasedImage,
+  EditedContext
 }
 
 @Component({
@@ -27,21 +27,24 @@ export class IndexComponent implements OnInit {
   LayerMode = LayerMode;
 
   layers = [
-    {mode: LayerMode.Image, name: '底圖' , checked: true},
-    {mode: LayerMode.Editor, name: '使用者作畫的內容', checked: true},
+    {mode: LayerMode.BasedImage, name: 'BasedImage' , label: '底圖' , checked: true},
+    {mode: LayerMode.EditedContext, name: 'EditedContext', label: '使用者作畫的內容' , checked: true},
   ];
 
   tools = [
     {mode: ToolMode.Line, name: '畫線' },
-    {mode: ToolMode.Square, name: '畫框' },
+    {mode: ToolMode.Rect, name: '畫框' },
     {mode: ToolMode.Circle, name: '畫圓' },
     {mode: ToolMode.Arrow, name: '箭頭' },
     {mode: ToolMode.Text, name: '文字' },
   ];
 
+  dragTrack = [];
+
   stage: Konva.Stage;
   imageLayer: Konva.Layer;
   editorLayer: Konva.Layer;
+  selectNode: Konva.Node;
 
   imageLayerLoad = false;
   selectedTool =  ToolMode.Line;
@@ -50,70 +53,192 @@ export class IndexComponent implements OnInit {
   lastLine;
 
 
-  constructor() {
+  constructor(
+    private ngZone: NgZone
+  ) {
   }
 
   ngOnInit() {
     this.stage = new Konva.Stage({container: 'container'});
-    this.imageLayer = new Konva.Layer();
-    this.editorLayer = new Konva.Layer();
+    this.imageLayer = new Konva.Layer({name: 'BasedImage'});
+    this.editorLayer = new Konva.Layer({name: 'EditedContext'});
     this.stage.add(this.imageLayer);
     this.stage.add(this.editorLayer);
-    this.initStage();
-
-    // this.stage.on('mousedown touchstart', (e) => {
-    //   this.isPaint = true;
-    //   const pos = this.stage.getPointerPosition();
-    //   this.lastLine = new Konva.Line({
-    //     stroke: '#000000',
-    //     strokeWidth: 5,
-    //     globalCompositeOperation: 'source-over',
-    //     points: [pos.x, pos.y]
-    //   });
-    //   this.editorLayer.add(this.lastLine);
-    // });
-
-    // this.stage.on('mouseup touchend', () => {
-    //   this.isPaint = false;
-    // });
-
-    // // and core function - drawing
-    // this.stage.on('mousemove touchmove', () => {
-    //   if (!this.isPaint) {
-    //     return;
-    //   }
-
-    //   const pos = this.stage.getPointerPosition();
-    //   const newPoints = this.lastLine.points().concat([pos.x, pos.y]);
-    //   this.lastLine.points(newPoints);
-    //   this.editorLayer.batchDraw();
-    // });
+    this.addStageListener();
   }
 
-  initStage() {
+  addStageListener() {
+
     this.stage.on('mousedown touchstart', () => {
-      this.isPaint = true;
-      const pos = this.stage.getPointerPosition();
-      switch (+this.selectedTool) {
-        case ToolMode.Text:
-          this.createTextNode(pos);
-          break;
-        default:
-          break;
-      }
+      this.startDragTrack();
     });
 
     this.stage.on('mouseup touchend', () => {
-      this.isPaint = false;
+      if (this.isPaint) {
+        switch (this.selectedTool) {
+          case ToolMode.Line:
+            this.createLineNode();
+            break;
+          case ToolMode.Arrow:
+            this.createArrowNode();
+            break;
+          case ToolMode.Rect:
+            this.createRectNode();
+            break;
+          case ToolMode.Circle:
+            this.createCircleNode();
+            break;
+          case ToolMode.Text:
+            this.createTextNode();
+            break;
+          default:
+            break;
+        }
+        this.isPaint = false;
+      }
     });
 
-    // and core function - drawing
     this.stage.on('mousemove touchmove', () => {
-      if (!this.isPaint) {
-        return;
-      }
-      const pos = this.stage.getPointerPosition();
+      this.updateDragTrack();
     });
+  }
+
+  startDragTrack() {
+    const pos = this.stage.getPointerPosition();
+    this.dragTrack = [{x: pos.x, y: pos.y}];
+    this.isPaint = true;
+  }
+
+  updateDragTrack() {
+    const pos = this.stage.getPointerPosition();
+    this.dragTrack.push({x: pos.x, y: pos.y});
+  }
+
+
+  getShapeInfo(dragTrack) {
+    const posStart = _.first(dragTrack);
+    const posEnd = _.last(dragTrack);
+    let r1x = posStart.x;
+    let r1y = posStart.y;
+    let r2x = posEnd.x;
+    let r2y = posEnd.y;
+    let d;
+    if (r1x > r2x ) {
+      d = Math.abs(r1x - r2x);
+      r1x = r2x;
+      r2x = r1x + d;
+    }
+    if (r1y > r2y ) {
+      d = Math.abs(r1y - r2y);
+      r1y = r2y;
+      r2y = r1y + d;
+    }
+    return ({x1: r1x, y1: r1y, x2: r2x, y2: r2y});
+  }
+
+  createRectNode() {
+    const shape = this.getShapeInfo(this.dragTrack);
+    const node = new Konva.Rect({
+      x: shape.x1,
+      y: shape.y1,
+      width: shape.x2 - shape.x1,
+      height: shape.y2 - shape.y1,
+      stroke: 'black',
+      strokeWidth: 2
+    });
+    this.editorLayer.add(node);
+    this.stage.batchDraw();
+  }
+
+  createCircleNode() {
+    const shape = this.getShapeInfo(this.dragTrack);
+    const node = new Konva.Circle({
+      x: shape.x1,
+      y: shape.y1,
+      radius: shape.x2 - shape.x1,
+      stroke: 'black',
+      strokeWidth: 2
+    });
+    this.editorLayer.add(node);
+    this.stage.batchDraw();
+  }
+
+  createLineNode() {
+    const trackPoints = _.flatMap(this.dragTrack, track => {
+      return [track.x , track.y];
+    });
+    const node = new Konva.Line({
+      points: trackPoints,
+      stroke: 'black',
+      tension: 0
+    });
+    this.editorLayer.add(node);
+    this.stage.batchDraw();
+  }
+
+  createArrowNode() {
+    this.dragTrack = [
+      _.first(this.dragTrack),
+      _.last(this.dragTrack),
+    ];
+    const trackPoints = _.flatMap(this.dragTrack, track => {
+      return [track.x , track.y];
+    });
+    const node = new Konva.Arrow({
+      points: trackPoints,
+      stroke: 'black',
+      tension: 0
+    });
+    this.editorLayer.add(node);
+    this.stage.batchDraw();
+  }
+
+  createTextNode() {
+    const container = document.querySelector('#container');
+    const shape = this.getShapeInfo(this.dragTrack);
+    const textNode = new Konva.Text({
+      text: 'Input text here',
+      x: shape.x1,
+      y: shape.y1,
+      fontSize: 20,
+      width: shape.x2 - shape.x1,
+      height: shape.y2 - shape.y1
+    });
+    this.editorLayer.add(textNode);
+    this.editorLayer.batchDraw();
+
+    textNode.on('dblclick', (e) => {
+      if (!this.isPaint) {
+        const node = e.currentTarget as Konva.Text;
+        // create textarea and style it
+        const textElement = document.createElement('textarea');
+        container.appendChild(textElement);
+        textElement.value = node.text();
+        textElement.style.position = 'absolute';
+        textElement.style.top = node.y() + 'px',
+        textElement.style.left = node.x() + 'px',
+        textElement.style.width = node.width() + 'px';
+        textElement.style.height = node.height() + 'px';
+        textElement.style.fontSize = 20 + 'px';
+        textElement.focus();
+
+        textElement.addEventListener('keydown', (evt) => {
+            // hide on enter
+            if (evt.keyCode === 13 || evt.keyCode === 27) {
+                const textArea = evt.target as HTMLTextAreaElement;
+                node.text(textArea.value);
+                this.editorLayer.draw();
+                container.removeChild(textArea);
+            }
+        });
+      }
+    });
+  }
+
+  resetContext() {
+    this.editorLayer.destroy();
+    this.editorLayer = new Konva.Layer({name: 'EditedContext'});
+    this.stage.add(this.editorLayer);
   }
 
   downloadURI(uri, name) {
@@ -149,83 +274,27 @@ export class IndexComponent implements OnInit {
     }
   }
 
+  onToolSelected(selectedTool) {
+    this.selectedTool = +selectedTool;
+  }
+
+  /**
+   * 圖層選擇事件
+   */
   onLayerSelected(selectedLayer) {
-    this.stage.getLayers().each(layer => layer.hide());
-    switch (+selectedLayer) {
-      case LayerMode.Image:
-        this.imageLayer.show();
-        break;
-      case LayerMode.Editor:
-        this.editorLayer.show();
-        break;
-      default:
-        this.stage.getLayers().each(layer => layer.show());
-        break;
+    selectedLayer.checked = !selectedLayer.checked;
+    const foundLayer = this.stage.findOne(layer => {
+      return layer.name() === selectedLayer.name;
+    }).visible(selectedLayer.checked);
+  }
+
+  isSaveAsImgDisabled() {
+    let isDisabled = false;
+    const count = _.filter(this.layers, layer => layer.checked).length;
+    if (count === 0) {
+      isDisabled = true;
     }
-  }
-
-  createArrowNode() {
-    console.log('createTextNode');
-    const arrow = new Konva.Arrow({
-      x: this.stage.width() / 4,
-      y: this.stage.height() / 4,
-      points: [0, 0, this.stage.width() / 2, this.stage.height() / 2],
-      pointerLength: 20,
-      pointerWidth: 20,
-      fill: 'black',
-      stroke: 'black',
-      strokeWidth: 4,
-      draggable: true,
-    });
-
-    this.editorLayer.add(arrow);
-    this.editorLayer.draw();
-  }
-
-  createTextNode(pos) {
-    console.log('createTextNode');
-    const textNode = new Konva.Text({
-      text: 'Input text here',
-      x: pos.x,
-      y: pos.y,
-      fontSize: 20,
-      draggable: true,
-      width: 200
-    });
-    this.editorLayer.add(textNode);
-    this.editorLayer.draw();
-
-    textNode.on('dblclick', () => {
-      // create textarea over canvas with absolute position
-
-      // first we need to find its position
-      const textPosition = textNode.getAbsolutePosition();
-      const stageBox = this.stage.container().getBoundingClientRect();
-
-      const areaPosition = {
-          x: textPosition.x + stageBox.left,
-          y: textPosition.y + stageBox.top
-      };
-
-      // create textarea and style it
-      const textarea = document.createElement('textarea');
-      document.body.appendChild(textarea);
-      textarea.value = textNode.text();
-      textarea.style.position = 'absolute';
-      textarea.style.top = areaPosition.y + 'px';
-      textarea.style.left = areaPosition.x + 'px';
-      textarea.style.width = textNode.width() + 'px';
-      textarea.focus();
-
-      textarea.addEventListener('keydown', (e) => {
-          // hide on enter
-          if (e.keyCode === 13 || e.keyCode === 27) {
-              textNode.text(textarea.value);
-              this.editorLayer.draw();
-              document.body.removeChild(textarea);
-          }
-      });
-    });
+    return isDisabled;
   }
 
 }
